4645ac414c068afd7a86ff815fd6fdaca3e83b1e
patrikduch <duchpatrik@icloud.com>
2021-03-17 08:50:54 +0100

    Net5: Project dto integration into REST controller

--- Net5WebAPI/Controllers/ProjectController.cs
+++ Net5WebAPI/Controllers/ProjectController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Net5WebAPI.Dtos;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Net5WebAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -14,34 +10,9 @@ namespace Net5WebAPI.Controllers
     {
         // GET: api/<ProjectController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ProjectDetail Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<ProjectController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ProjectController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ProjectController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ProjectController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new ProjectDetail("Project name");
         }
     }
 }
